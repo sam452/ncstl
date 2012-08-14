9e9a6d9829060717d6b881724f980f7418347cc2
sam walton <sam@foofrancine.com>
2012-08-14 13:49:05 -0500

    strip down css

--- wp-content/themes/train/footer.php
+++ wp-content/themes/train/footer.php
@@ -1,13 +1,5 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content
- * after. Calls sidebar-footer.php for bottom widgets.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
  */
 ?>
 	</div><!-- #main -->
@@ -19,24 +11,18 @@
 	/* A sidebar in the footer? Yep. You can can customize
 	 * your footer with four columns of widgets.
 	 */
-	get_sidebar( 'footer' );
 ?>
 
 			<div id="site-info">
-				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>
 			</div><!-- #site-info -->
 
-			<div id="site-generator">
-				<?php do_action( 'twentyten_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
+			<div id="copyright">
 			</div><!-- #site-generator -->
 
 		</div><!-- #colophon -->
 	</div><!-- #footer -->
 
-</div><!-- #wrapper -->
+<!--</div><!-- #wrapper -->
 
 <?php
 	/* Always have wp_footer() just before the closing </body>
