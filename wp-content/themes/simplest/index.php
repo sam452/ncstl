4e9021bb996a8007de730a1ab1e1e0b187de1dd3
sam walton <sam@foofrancine.com>
2012-08-16 09:27:23 -0500

    blocked out footer

--- wp-content/themes/simplest/index.php
+++ wp-content/themes/simplest/index.php
@@ -55,8 +55,30 @@
         <?php endif; ?>
         <div class="clear"> </div>
       </div><!-- content -->
-      <footer>Footer content here</footer>
+      
+      
+      <footer class="clearfix">
+        <div class="bLogo"><img src="<?php echo get_template_directory_uri(); ?>/img/bowtie.gif" width="128" height="72" alt="NCStL logo" /><span class="typemin">
+        NC&StL Preservation Society, Inc. is in no way affiliated with the NC&StL Railway or any of its successors. As a non-profit entity, NCPS presents these pages to the public purely for educational and historic interest.
+        </span>
+        </div>
+        <div class="footerLinks">links here1</div>
+        <div class="footerLinks">links here2</div>
+        <div class="pPal">Please take a moment to send a tax-deductible contribution to NCPS.
+          <br>
+            We can't do it without your help!
+          <br>
+          <form method="post" action="https://www.paypal.com/cgi-bin/webscr">
+          <input type="hidden" value="_s-xclick" name="cmd">
+          <input type="hidden" value="D5NRKJGL2PZC6" name="hosted_button_id">
+          <input type="image" border="0" alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif">
+          <img width="1" border="0" height="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt="">
+          </form>
+        </div>
+      </footer>
     </div><!-- container -->
     <?php wp_footer(); ?>
+    <div class="copyright">&copy;2012, NC&amp;StL Preservation Society
+			</div>
   </body>
 </html>
