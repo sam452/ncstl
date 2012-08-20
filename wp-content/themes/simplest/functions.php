ceccfde1bdecc0b8e20d5599df7e087d5f55b9ae
sam walton <sam@foofrancine.com>
2012-08-20 10:40:17 -0500

    first attempt at custom Train post.

--- wp-content/themes/simplest/functions.php
+++ wp-content/themes/simplest/functions.php
@@ -14,15 +14,35 @@
     add_action( 'init', 'register_my_menu' );
     function register_my_menu() {
       register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
-    }
+      
+  add_action( 'init', 'create_my_post_types' );
+    function create_my_post_types() {
+      register_post_type( 'Railroad', 
+		array(
+		       'labels' => array(
+		         'name' => __( 'Train pages' ),
+		      'singular_name' => __( 'Train page' ),
+		      'add_new' => __( 'Add New' ),
+		      'add_new_item' => __( 'Add New Train Page' ),
+		      'edit' => __( 'Edit' ),
+		      'edit_item' => __( 'Edit Train Page' ),
+		      'new_item' => __( 'New Train Page' ),
+		      'view' => __( 'View Train Page' ),
+		      'view_item' => __( 'View Train Page' ),
+		      'search_items' => __( 'Search Railroad Pages' ),
+		      'not_found' => __( 'No Railroad pages found' ),
+		      'not_found_in_trash' => __( 'No Railroad pages found in Trash' ),
+		      'parent' => __( 'History' )
+			 ),
+			'menu_icon' => get_stylesheet_directory_uri() . '/img/train-button-image.gif',
+		)
+	  );
+     }    //end of custom pages
+    
 
 
 
 
 
-
-
-
-
-
+}//end of php call
 ?>
