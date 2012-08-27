0812a3d703b7801fb58c8f986c9b40d26484975e
sam walton <sam@foofrancine.com>
2012-08-27 15:16:08 -0500

    third attempt at db cleanup. css is changed.

--- wp-content/themes/simplest/header.php
+++ wp-content/themes/simplest/header.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -10,84 +10,26 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
+<div id='wrapper'>
 
-      <div id="header" class="chrome-center"> <!--3:25pm added to test header-->
+  <div id='container'>
+      
+      <div id="header" class="chrome-center">
+      <div class="bLogo1"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpg" width="100%" height="100%" alt="NCStL logo" /></div>
+    
+    <div id='search'>
+      <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+      <div><input type="text" size="18" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+      <input type="submit" id="searchsubmit" value="Search" class="btn" />
+      </div>
+      </form>
+    </div><!--#search-->
 
-
-       <!-- removed extra header -->
-
-
-  <div class="bLogo"><img src="<?php echo get_template_directory_uri(); ?>/img/bowtie.gif" width="100%" height="72" alt="NCStL logo" />
-    </div>
-    <div class="576Logo"><img src="<?php echo get_template_directory_uri(); ?>/img/576park_da.jpg" width="77%" height="100" alt="576 logo" />
-        </div>
-
-  <div class="bloginfo">
-        <h1 style="text-align: center;"><?php bloginfo( 'name' ); ?></h1>
-              <p id="description"><?php bloginfo( 'description' ); ?></p>
-    </div> <!--- blog info div -->
-        
-      <!--<nav>
+    <nav>    
       <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ) ?>
-      </nav>-->
+    </nav>
 
-      <div id="nav">
-  <ul>
-    <li><a href="#">HOME</a></li>
-    <li><a href="#">ABOUT US</a>
-      <ul>
-        <li><a href="http://www.ncstl.com/general/contact.htm">CONTACT US</a></li>
-        <li><a href="#">JOIN NCPS</a></li>
-        <li><a href="#">PRIVACY POLICY</a></li>
-        <li><a href="#">LINKS</a>
-          <ul>
-            <li><a href="#">another site</a></li>
-            <li><a href="#">another site</a></li>
-          </ul>
-        </li>
-      </ul>
-    </li>
-    <li><a href="#">HISTORY</a>
-      <ul>
-        <li><a href="#">CITIES/TRACKS</a></li>
-        <li><a href="#">THE 576</a></li>
-        <li><a href="#">MEMORABILIA</a></li>
-        <li><a href="#">STORIES</a></li>
-        <li><a href="#">ARTICLES</a></li>
-        <li><a href="#">SKETCH</a></li>
-      </ul>
-    </li>
-    <li><a href="#">PEOPLE</a></li>
-    <li><a href="#">LOCOMOTIVES</a>
-      <ul>
-        <li><a href="#">THE 576</a></li>
-        <li><a href="#">STEAM ENGINES</a></li>
-        <li><a href="#">DIESEL ENGINES</a></li>
-        <li><a href="#">PASSENGER TRAINS</a></li>
-        <li><a href="#">FREIGHT TRAINS</a></li>
-        <li><a href="#">1939 ROSTER</a></li>
-        <li><a href="#">ROLLING STOCK</a></li>
-        <li><a href="#">MAINTENANCE EQUIPMENT</a></li>
-      </ul>
-    </li>
-    <li><a href="http://www.ncstl.com/merchandise/index.html">MERCHANDISE</a></li>
-    <li><a href="#">EVENTS</a>
-      <ul>
-        <li><a href="#">UPCOMING EVENTS</a></li>
-        <li><a href="#">PAST EVENTS</a></li>
-      </ul>
-    </li>
-    <li><a href="#">MEMBERS ONLY</a>
-      <ul>
-        <li><a href="http://www.ncstl.com/membership/membership.htm">LOG IN</a></li>
-        <li><a href="#">INDEX</a></li>
-        <li><a href="#">NEWLY ADDED</a></li>
-        <li><a href="#">NEWSLETTERS</a></li>
-        <LI><a href="#">EMPLOYEE DATABASE</a></li>
-      </ul>
-    </li>
-  </ul>
+      </div><!--end of header-->
+    
 
-</div>
-       
-</div><!--end of header-->
+   
