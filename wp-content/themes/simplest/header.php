01d4107e5ff1b8645773d83b8710c7cdee25bf9c
Chris <lunarsunrise@msn.com>
2012-08-28 10:33:07 -0500

    New header

--- wp-content/themes/simplest/header.php
+++ wp-content/themes/simplest/header.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
@@ -26,10 +27,140 @@
     </div><!--#search-->
 
     <nav>    
+=======
+     <!-- 8-28 hide <div id="header-container">--><!--3:25pm added to test header container-->
+      <div id="header" class="chrome-center"> <!--3:25pm added to test header-->
+      <!-- added 8-28--><div id="header"
+
+       <!-- removed extra header -->
+
+
+ <!--8-27 hide <div class="bLogo"><img src="<?php echo get_template_directory_uri(); ?>/img/bowtie.gif" width="100%" height="72" alt="NCStL logo" />-->
+    <!--8-27 hide</div>-->
+    <!-- 8-27 hide <div class="576Logo"><img src="<?php echo get_template_directory_uri(); ?>/img/576park_da.jpg" width="77%" height="100" alt="576 logo" />-->
+        <!--8-27 hide </div>-->
+
+  <!-- hide to make nav bar fit 8-27 <div class="bloginfo">
+        <h1 style="text-align: center;"><?php bloginfo( 'name' ); ?></h1>
+              <p id="description"><?php bloginfo( 'description' ); ?></p>
+    </div> --><!--- blog info div -->
+        
+      <!--<nav>
+>>>>>>> New header
       <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'nav', 'theme_location' => 'primary-menu' ) ) ?>
     </nav>
 
+<<<<<<< HEAD
       </div><!--end of header-->
     
+=======
+
+      <!--8-27 new--><div id="header_h">
+      <div class="bowtie"><img src="<?php echo get_template_directory_uri(); ?>/img/bowtie.gif" width="200" height="" alt="bowtie" />
+        </div>
+<!--<div class="left_h">
+<a title="logo" href="http://www.ncstl.com">
+    <img class="first" alt="NCPS" src="img/bowtie.gif">
+</a>
+</div>-->
+<div class="center">
+
+<h1>
+Nashville, Chattanooga & St Louis Railway
+<br>
+Preservation Society
+</h1>
+
+</div>
+<div class="eastwood"><img src="<?php echo get_template_directory_uri(); ?>/img/57649.jpg" width="300" height="" alt="576" />
+        </div>
+<!--<div class="eastwood">
+<img width="310" height="156" border="0" src="img/57649.jpg" alt="Logo of The Nashville, Chattanooga & St Louis Railway Preservation Society">
+</div>-->
+
+<div class="omega">
+<ul>
+<li>
+  <img src="<?php echo get_template_directory_uri(); ?>/img/donate.png" width="" height="" alt="Donate" />
+        </li>
+<!--<a title="Link to donate to NC&StL" href="/Donate">
+<img alt="Donate" src="img/donate.png">
+</a>-->
+<li>
+  <img src="<?php echo get_template_directory_uri(); ?>/img/join.png" width="" height="" alt="Join" />
+        </li>
+<!--<a title="Link to Membership" href="/Membership">-->
+<!--<img alt="Join" src="img/join.png">
+</a>-->
+<!--</li>-->
+  <li>
+  <img src="<?php echo get_template_directory_uri(); ?>/img/shop.png" width="" height="" alt="Shop" />
+     </li> 
+  <!--<a title="Link to Shop" href="/Shop">
+  <img alt="Shop" src="img/shop.png">
+</a>-->
+</div>
+</ul>
+</div>
+</div>
+
+
+      <div id="nav">
+  <ul>
+    <li><a href="#">HOME</a></li>
+    <li><a href="#">ABOUT US</a>
+      <ul>
+        <li><a href="http://www.ncstl.com/general/contact.htm">CONTACT US</a></li>
+        <li><a href="#">JOIN NCPS</a></li>
+        <li><a href="#">PRIVACY POLICY</a></li>
+        <li><a href="#">LINKS</a>
+          <ul>
+            <li><a href="#">another site</a></li>
+            <li><a href="#">another site</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a href="#">HISTORY</a>
+      <ul>
+        <li><a href="#">CITIES/TRACKS</a></li>
+        <li><a href="#">THE 576</a></li>
+        <li><a href="#">MEMORABILIA</a></li>
+        <li><a href="#">STORIES</a></li>
+        <li><a href="#">ARTICLES</a></li>
+        <li><a href="#">SKETCH</a></li>
+      </ul>
+    </li>
+    <li><a href="#">PEOPLE</a></li>
+    <li><a href="#">LOCOMOTIVES</a>
+      <ul>
+        <li><a href="#">THE 576</a></li>
+        <li><a href="#">STEAM ENGINES</a></li>
+        <li><a href="#">DIESEL ENGINES</a></li>
+        <li><a href="#">PASSENGER TRAINS</a></li>
+        <li><a href="#">FREIGHT TRAINS</a></li>
+        <li><a href="#">1939 ROSTER</a></li>
+        <li><a href="#">ROLLING STOCK</a></li>
+        <li><a href="#">MAINTENANCE EQUIPMENT</a></li>
+      </ul>
+    </li>
+    <li><a href="http://www.ncstl.com/merchandise/index.html">MERCHANDISE</a></li>
+    <li><a href="#">EVENTS</a>
+      <ul>
+        <li><a href="#">UPCOMING EVENTS</a></li>
+        <li><a href="#">PAST EVENTS</a></li>
+      </ul>
+    </li>
+    <li><a href="#">MEMBERS ONLY</a>
+      <ul>
+        <li><a href="http://www.ncstl.com/membership/membership.htm">LOG IN</a></li>
+        <li><a href="#">INDEX</a></li>
+        <li><a href="#">NEWLY ADDED</a></li>
+        <li><a href="#">NEWSLETTERS</a></li>
+        <LI><a href="#">EMPLOYEE DATABASE</a></li>
+      </ul>
+    </li>
+  </ul>
+>>>>>>> New header
 
    
